dd3ab34dca3a4e690a8a69244ecb6942d6f31801
Eloyse Fernanda <eloyse.fernanda@g.globo>
2026-04-28 15:41:35 -0300

    fix: refactor driver manager and use hikari connection pool

--- biblioteca/src/main/java/util/ConexaoDB.java
+++ biblioteca/src/main/java/util/ConexaoDB.java
@@ -1,25 +1,41 @@
 package util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public final class ConexaoDB {
-    private static final Properties PROPERTIES = new Properties();
+    private static final HikariDataSource dataSource;
 
     static {
+        Properties props = new Properties();
         try (InputStream input = ConexaoDB.class.getClassLoader().getResourceAsStream("db.properties")) {
             if (input == null) {
                 throw new IllegalStateException("Arquivo db.properties nao encontrado no classpath.");
             }
+            props.load(input);
 
-            PROPERTIES.load(input);
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (IOException | ClassNotFoundException e) {
-            throw new ExceptionInInitializerError(e);
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(props.getProperty("db.url"));
+            config.setUsername(props.getProperty("db.user"));
+            config.setPassword(props.getProperty("db.password"));
+            config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+            // Configurações do pool
+            config.setMaximumPoolSize(10);
+            config.setMinimumIdle(2);
+            config.setIdleTimeout(30000);
+            config.setConnectionTimeout(30000);
+            config.setMaxLifetime(1800000);
+
+            dataSource = new HikariDataSource(config);
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError("Erro ao carregar db.properties: " + e.getMessage());
         }
     }
 
@@ -27,11 +43,7 @@ public final class ConexaoDB {
     }
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(
-                PROPERTIES.getProperty("db.url"),
-                PROPERTIES.getProperty("db.user"),
-                PROPERTIES.getProperty("db.password")
-        );
+        return dataSource.getConnection();
     }
 }
 
